09944424275fab6cca0997d059fc4ff504ba1e11
yaser-uwu <destructor12342@gmail.com>
2025-03-28 23:05:19 -0500

    Cambio RTv

--- ModuloDeCompra_BD/Formulario/FrmDevolucionesRTV.Designer.cs
+++ ModuloDeCompra_BD/Formulario/FrmDevolucionesRTV.Designer.cs
@@ -46,9 +46,13 @@
             this.txtSeleccionGRN = new Guna.UI2.WinForms.Guna2TextBox();
             this.btnSeleccionarGRN = new Guna.UI2.WinForms.Guna2Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.guna2Panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleGrn)).BeginInit();
+            this.guna2Panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblEstadoOrden
@@ -86,6 +90,8 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.guna2Panel2);
             this.panel1.Controls.Add(this.btnCancelar);
             this.panel1.Controls.Add(this.btnRegistrarRTV);
             this.panel1.Controls.Add(this.label2);
@@ -96,7 +102,7 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1362, 395);
+            this.panel1.Size = new System.Drawing.Size(1362, 639);
             this.panel1.TabIndex = 2;
             // 
             // btnCancelar
@@ -108,7 +114,7 @@
             this.btnCancelar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnCancelar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(351, 337);
+            this.btnCancelar.Location = new System.Drawing.Point(264, 586);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(122, 34);
             this.btnCancelar.TabIndex = 7;
@@ -123,7 +129,7 @@
             this.btnRegistrarRTV.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.btnRegistrarRTV.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegistrarRTV.ForeColor = System.Drawing.Color.White;
-            this.btnRegistrarRTV.Location = new System.Drawing.Point(180, 337);
+            this.btnRegistrarRTV.Location = new System.Drawing.Point(103, 586);
             this.btnRegistrarRTV.Name = "btnRegistrarRTV";
             this.btnRegistrarRTV.Size = new System.Drawing.Size(122, 34);
             this.btnRegistrarRTV.TabIndex = 6;
@@ -135,7 +141,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label2.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(48, 149);
+            this.label2.Location = new System.Drawing.Point(128, 183);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 17);
             this.label2.TabIndex = 5;
@@ -152,7 +158,7 @@
             this.txtProveedor.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txtProveedor.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txtProveedor.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtProveedor.Location = new System.Drawing.Point(49, 169);
+            this.txtProveedor.Location = new System.Drawing.Point(129, 203);
             this.txtProveedor.Name = "txtProveedor";
             this.txtProveedor.PlaceholderText = "";
             this.txtProveedor.ReadOnly = true;
@@ -167,9 +173,9 @@
             this.guna2Panel1.Controls.Add(this.lbCantRecib);
             this.guna2Panel1.Controls.Add(this.dgvDetalleGrn);
             this.guna2Panel1.Controls.Add(this.label3);
-            this.guna2Panel1.Location = new System.Drawing.Point(180, 46);
+            this.guna2Panel1.Location = new System.Drawing.Point(103, 237);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(1164, 285);
+            this.guna2Panel1.Size = new System.Drawing.Size(1164, 343);
             this.guna2Panel1.TabIndex = 3;
             // 
             // lbCantRecib
@@ -177,7 +183,7 @@
             this.lbCantRecib.AutoSize = true;
             this.lbCantRecib.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.lbCantRecib.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbCantRecib.Location = new System.Drawing.Point(716, 17);
+            this.lbCantRecib.Location = new System.Drawing.Point(714, 34);
             this.lbCantRecib.Name = "lbCantRecib";
             this.lbCantRecib.Size = new System.Drawing.Size(216, 17);
             this.lbCantRecib.TabIndex = 8;
@@ -206,13 +212,13 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvDetalleGrn.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvDetalleGrn.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.dgvDetalleGrn.Location = new System.Drawing.Point(27, 40);
+            this.dgvDetalleGrn.Location = new System.Drawing.Point(26, 66);
             this.dgvDetalleGrn.MultiSelect = false;
             this.dgvDetalleGrn.Name = "dgvDetalleGrn";
             this.dgvDetalleGrn.RowHeadersVisible = false;
             this.dgvDetalleGrn.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dgvDetalleGrn.RowTemplate.Height = 45;
-            this.dgvDetalleGrn.Size = new System.Drawing.Size(1115, 228);
+            this.dgvDetalleGrn.Size = new System.Drawing.Size(1115, 262);
             this.dgvDetalleGrn.TabIndex = 7;
             this.dgvDetalleGrn.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
             this.dgvDetalleGrn.ThemeStyle.AlternatingRowsStyle.Font = null;
@@ -245,7 +251,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.label3.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(415, 7);
+            this.label3.Location = new System.Drawing.Point(417, 15);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(184, 17);
             this.label3.TabIndex = 6;
@@ -262,7 +268,7 @@
             this.txtSeleccionGRN.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txtSeleccionGRN.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txtSeleccionGRN.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtSeleccionGRN.Location = new System.Drawing.Point(49, 91);
+            this.txtSeleccionGRN.Location = new System.Drawing.Point(130, 142);
             this.txtSeleccionGRN.Name = "txtSeleccionGRN";
             this.txtSeleccionGRN.PlaceholderText = "";
             this.txtSeleccionGRN.ReadOnly = true;
@@ -279,7 +285,7 @@
             this.btnSeleccionarGRN.FillColor = System.Drawing.Color.Purple;
             this.btnSeleccionarGRN.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSeleccionarGRN.ForeColor = System.Drawing.Color.White;
-            this.btnSeleccionarGRN.Location = new System.Drawing.Point(31, 46);
+            this.btnSeleccionarGRN.Location = new System.Drawing.Point(112, 97);
             this.btnSeleccionarGRN.Name = "btnSeleccionarGRN";
             this.btnSeleccionarGRN.Size = new System.Drawing.Size(122, 39);
             this.btnSeleccionarGRN.TabIndex = 1;
@@ -296,6 +302,35 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "DEVOLUCIONES DE PRODUCTOS  (RTV)";
             // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BorderColor = System.Drawing.Color.Black;
+            this.guna2Panel2.BorderThickness = 2;
+            this.guna2Panel2.Controls.Add(this.txtMotivo);
+            this.guna2Panel2.Location = new System.Drawing.Point(254, 113);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(1013, 117);
+            this.guna2Panel2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(648, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Motivo de devolución";
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(20, 13);
+            this.txtMotivo.Multiline = true;
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(970, 90);
+            this.txtMotivo.TabIndex = 0;
+            // 
             // FrmRTV
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -311,6 +346,8 @@
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleGrn)).EndInit();
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -331,5 +368,8 @@
         private Guna.UI2.WinForms.Guna2TextBox txtSeleccionGRN;
         private Guna.UI2.WinForms.Guna2Button btnSeleccionarGRN;
         private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMotivo;
     }
 }
